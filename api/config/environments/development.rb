b9c1b80815dbaa6ce17ff602d26377511d283634
Edvard <edvbot@gmail.com>
2021-12-12 10:40:12 +0100

    Renamed cache variable names; removed development secret.txt from tracking

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :redis_cache_store, { url: ENV.fetch('CACHE_REDIS_URL'){ "redis://localhost:6379/1" }, namespace: 'srv::cache' }
+    config.cache_store = :redis_cache_store, { url: ENV.fetch('CACHE_URL'){ "redis://localhost:6379/1" }, namespace: 'srv::cache' }
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
--- api/config/initializers/sidekiq.rb
+++ api/config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-sidekiq_config = { url: ENV.fetch('SIDEKIQ_REDIS_URL'){ "redis://localhost:6379/0" } }
+sidekiq_config = { url: ENV.fetch('JOB_WORKER_URL'){ "redis://localhost:6379/0" } }
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
