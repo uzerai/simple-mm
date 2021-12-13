5e033d8168d85699223a85501905510f330f1c56
Edvard <edvbot@gmail.com>
2021-12-13 01:05:52 +0100

    added web-app for learning vue; added dockerfile and docker-compose changes to serve front-end via nginx; updated .tool-versions to include npm

---
+++ web/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div className="grid place-items-center h-screen">
+    <div cassName="bg-blue-200">
+      <p>Hello world</p>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
