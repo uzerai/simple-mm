8a1fa13270003adc6d06c76a2dd968ff291f7f1e
Edvard <edvbot@gmail.com>
2021-12-13 22:48:04 +0100

    first data-backed component, simple functionality

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div className="grid place-items-center h-screen">
-    <div cassName="bg-blue-200">
-      <p>Hello world</p>
+    <div className="bg-blue-200 p-8 rounded-lg">
+      <h1 className="text-3xl font-bold text-gray-900">Simple Matchmaking</h1>
+      <hr className="my-2 border-gray-700"/>
+      <PlayerTable />
     </div>
   </div>
 </template>
 
 <script>
+  import PlayerTable from './components/PlayerTable.vue'
+
+  export default {
+    name: 'App',
+    props: {},
+    components: {
+      PlayerTable
+    }
+  }
 </script>
+
---
+++ web/src/components/PlayerTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <table className="min-w-full">
+    <thead className="bg-gray-100">
+      <tr>
+        <th className="text-left text-l tracking-wider" scope="col">
+          Username
+        </th>
+        <th className="text-left text-l tracking-wider" scope="col">
+          Rating
+        </th>
+      </tr>
+    </thead>
+    <tbody className="divide-y divide-gray-500">
+      <tr v-for='player in players' :key="player.id">
+        <td>{{ player.username }}</td>
+        <td className="text-center">{{ player.rating }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'PlayerTable',
+    data () {
+      return { 
+        players: []
+      }
+    },
+    async created () {
+      const request = await fetch("http://localhost:8010/players", {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      if(request.ok) {
+         this.players = await request.json()
+      }
+    },
+  }
+
+  
+</script>
