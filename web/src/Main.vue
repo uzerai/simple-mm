410693154afd0cee0332935ceb09efcbb062292b
edvard <edvard@splay.com>
2021-12-18 16:26:08 +0100

    updated dependencies to latest, now supports TailwindCSS@3+, added eslintconfig to work with vue internal webpack config, avoids issue with import()/require() implementations

--- web/src/Main.vue
+++ web/src/Main.vue
@@ -2,7 +2,12 @@
   Super simple main component for handling routing throughout the application.
 -->
 <template>
-  <router-view></router-view>
+  <div id="main" class="">
+    <div id="top-bar" class="w-full h-10 bg-black">
+
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,5 +1,7 @@
 import App from './components/App';
+import PlayerTable from './components/PlayerTable';
 
 export default [
-	{ path: '', component: App }
+	{ path: '', component: App },
+	{ path: '/players', component: PlayerTable }
 ]
