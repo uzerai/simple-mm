e2041c728e1faf224e8a0702b674fac02bd446d9
edvard <edvard@splay.com>
2022-01-02 19:01:24 +0100

    temporary but more pleasing version of homepage

--- web/src/pages/Home.vue
+++ web/src/pages/Home.vue
@@ -1,7 +1,9 @@
 <template>
-  <content>
-    <h1 class="text-2xl">Home</h1>
+  <div class="w-full h-screen absolute -top-16 pointer-events-none">
+  <content id="absolute-centered" class="w-full h-full flex items-center justify-center">
+    <h1 class="text-2xl text-gray-700 italic font-bold">Home</h1>
   </content>
+  </div>
 </template>
 
 <script>
