4e678ac979c28ff3f5c5bcfaafeebf5ae1ed61e3
Edvard <edvbot@gmail.com>
2022-02-12 23:03:53 +0100

    updated eslintrc

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -4,12 +4,14 @@ module.exports = {
     es2021: true,
   },
   extends: [
-  'eslint:recommended',
-  'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
   plugins: ["vue"],
-  rules: {},
+  rules: {
+    "vue/prop-name-casing": ["warn", "snake_case"]
+  },
 };
